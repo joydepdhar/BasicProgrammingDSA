680428ced60991ab61fd9259c9e35a6edd870717
joydepdhar <joydepdhar138@gmai.com>
2025-03-31 22:50:30 +0600

    pushFront function update

--- BasicProgramming/doublyLL.cpp
+++ BasicProgramming/doublyLL.cpp
@@ -1,26 +1,55 @@
 #include<iostream>
 using namespace std;
-class Node{
-    public:
+
+class Node {
+public:
     int data;
     Node* next;
     Node* prev;
-    Node(int val){
-        data=val;
-        next=prev=NULL;
+    Node(int val) {
+        data = val;
+        next = prev = NULL;
     }
 };
 
-class DoublyLL{
+class DoublyLL {
     Node* head;
     Node* tail;
-    public:
-    DoublyLL(){
-        head=tail=NULL;
+public:
+    DoublyLL() {
+        head = tail = NULL;
+    }
+
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    void push_back(int val){
+        
+    }
+
+    void print() {
+        Node* temp = head;  // Use temp instead of head
+        while (temp != NULL) {  // Check temp instead of head
+            cout << temp->data << " ";  // Add space for readability
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
     }
 };
 
-int main(){
-    
+int main() {
+    DoublyLL dl;
+    dl.push_front(3);
+    dl.push_front(2);
+    dl.push_front(1);
+    dl.print();  // Missing semicolon fixed
 
+    return 0;
 }
